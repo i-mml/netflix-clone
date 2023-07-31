85cc7bb259ce685d019bbe02e747c1a3ac34e96f
maede <maedekarimi69110@gmail.com>
2023-07-31 18:17:27 +0330

    handle tp box and promotion box in home view

---
+++ views/homeView/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopBox from "./components/topBox";
+
+const HomeView = () => {
+  return (
+    <main>
+      <TopBox />
+    </main>
+  );
+};
+
+export default HomeView;
